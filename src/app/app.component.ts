fdf8a0f6d9e59d6d10c3a139d50c7f471bc0bd3c
IkerMadariaga <iker.moreno002@gmail.com>
2025-04-08 09:53:46 +0200

    0.1.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -6,7 +6,7 @@ import { TranslateService } from '@ngx-translate/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'codeTrek';
@@ -15,13 +15,13 @@ export class AppComponent {
   constructor(private router: Router, private translate: TranslateService) {
     this.translate.setDefaultLang('en'); // Idioma por defecto
   }
-  
+
   dropdownOpen = false;
   selectedLanguage: { flag: string } | null = null; // Empieza sin un idioma seleccionado
 
   languages = [
     { code: 'es', flag: 'assets/spain-flag-png.webp' },
-    { code: 'en', flag: 'assets/usa-flag.png' }
+    { code: 'en', flag: 'assets/usa-flag.png' },
   ];
 
   navigateToLogin() {
@@ -42,19 +42,40 @@ export class AppComponent {
 
   cambiarIdioma(languageCode: string) {
     // Cambiar el idioma seleccionado
-    const selectedLanguage = this.languages.find(lang => lang.code === languageCode);
-    
+    const selectedLanguage = this.languages.find(
+      (lang) => lang.code === languageCode
+    );
+
     // Cambiar el texto del botón al idioma seleccionado
     if (selectedLanguage) {
       this.selectedLanguage = { flag: selectedLanguage.flag };
     }
 
     // Cambiar el idioma en la aplicación
-    this.translate.use(languageCode);  // Aquí es donde se cambia el idioma
+    this.translate.use(languageCode); // Aquí es donde se cambia el idioma
     console.log('Idioma seleccionado:', languageCode);
   }
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+
+    if (target.closest('#menu-toggle-btn')) {
+      return;
+    }
+
+    // Si el menú está abierto
+    if (this.isMenuOpen) {
+      const clickedInsideMenu = target.closest('#sidebar-menu');
+
+      // Si NO se hizo clic dentro del menú
+      if (!clickedInsideMenu) {
+        this.isMenuOpen = false;
+      }
+    }
   }
 }
